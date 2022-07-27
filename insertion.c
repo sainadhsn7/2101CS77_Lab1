d06b4657b746e5c12d6ad9e195877d5e08b2b20a
sainadhsn7 <95209666+sainadhsn7@users.noreply.github.com>
2022-07-27 23:07:11 +0530

    Create insertion.c

---
+++ insertion.c
@@ -0,0 +1,44 @@
+#include <math.h>
+#include <stdio.h>
+#include<stdlib.h>
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    
+    return 0;
+}
