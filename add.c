ca28fe92475b132795d8ac9cba2d42b2c9046896
sainadhsn7 <95209666+sainadhsn7@users.noreply.github.com>
2022-07-27 23:23:34 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x,y;
+    printf("Enter your two numbers X and Y\n");
+    
+    printf("X:");
+    scanf("%d",&x);
+
+    printf("Y:");
+    scanf("%d",&y);
+
+    printf("X + Y = %d",x+y);
+    return 0;
+}
