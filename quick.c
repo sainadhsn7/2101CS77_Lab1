704838db15a035c0213906bdcca98824bed79257
sainadhsn7 <95209666+sainadhsn7@users.noreply.github.com>
2022-07-27 23:21:50 +0530

    Create quick.c

---
+++ quick.c
@@ -0,0 +1,51 @@
+#include <math.h>
+#include <stdio.h>
+#include<stdlib.h>
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+v
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int partition(int array[], int low, int high) {
+
+
+  int pivot = array[high];
+  int i = (low - 1);
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+      i++;
+      swap(&array[i], &array[j]);
+    }
+  }
+  swap(&array[i + 1], &array[high]);
+  return (i + 1);
+}
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+    int pi = partition(array, low, high);
+    quickSort(array, low, pi - 1);
+    quickSort(array, pi + 1, high);
+  }
+}
+
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    quickSort(arr, 0, n - 1);
+    printf("Sorted array in ascending order: \n");
+    printArray(arr, n);
+
+    return 0;
+}
